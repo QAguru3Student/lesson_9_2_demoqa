5fa6e6479c7252c0f8cb76efc600ae3cedec5018
Babushkin Anton <napishi_antonu@mail.ru>
2022-03-26 00:00:33 +0300

    browser config

--- src/test/java/ant/one/tests/TestBase.java
+++ src/test/java/ant/one/tests/TestBase.java
@@ -4,10 +4,16 @@ import ant.one.pages.RegistrationsPage;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 
+
 public class TestBase {
 
     RegistrationsPage registrationsPage = new RegistrationsPage();
 
+    @BeforeAll
+    static void config() {
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "2100x1080";
+    }
 
 }
 
