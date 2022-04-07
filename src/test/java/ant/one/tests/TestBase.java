6a83af82571a81885035a3bcc9071c9c52e74885
Babushkin Anton <napishi_antonu@mail.ru>
2022-04-07 18:42:33 +0300

    added selenide listener

--- src/test/java/ant/one/tests/TestBase.java
+++ src/test/java/ant/one/tests/TestBase.java
@@ -2,7 +2,10 @@ package ant.one.tests;
 
 import ant.one.pages.RegistrationsPage;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 
 public class TestBase {
@@ -11,10 +14,18 @@ public class TestBase {
 
     @BeforeAll
     static void config() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browser = "chrome";
         Configuration.browserSize = "2100x1080";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+
+
     }
 
 }
